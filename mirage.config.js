c1199a31dbf933f7560c35a381665d3848adc41f
augustopiatto <ampiatto@hotmail.com>
2025-09-13 14:03:01 -0300

    build: add mirage mock to project

---
+++ mirage.config.js
@@ -0,0 +1,27 @@
+import { createServer } from 'miragejs'
+
+export function makeServer() {
+  if (import.meta.env.MODE === 'development' && !window.server) {
+    const routes = []
+
+    window.server = createServer({
+      models: {},
+
+      routes() {
+        this.urlPrefix = 'http://localhost:8080'
+        this.namespace = 'api'
+        this.timing = 400
+
+        routes.forEach((route) => {
+          this[route.method](route.url, (_, request) => route.result(request))
+        })
+
+        this.passthrough()
+      },
+
+      seeds() {},
+    })
+
+    return window.server
+  }
+}
