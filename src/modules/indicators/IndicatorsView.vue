9ae4383a66b9a728f3ec6e62a2f7cd9b6e28954b
TiagoBernardoSantos <tiagobsantos154@gmail.com>
2025-09-27 17:27:07 -0300

    feat: RAD-22/setup-routing-for-menu-options-and-interface

---
+++ src/modules/indicators/IndicatorsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Indicadores</h1>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.indicators-view{
+    display: flex;
+  flex-direction: column;
+  gap: 12px;
+}
+</style>
