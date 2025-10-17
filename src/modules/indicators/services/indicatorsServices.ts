266d3421bde27a251b9dbd477da3da21dbe78301
Augusto de Moraes Piatto <ampiatto@hotmail.com>
2025-10-16 21:23:07 -0300

    feat: adjust url name for criteria

--- src/modules/indicators/services/indicatorsServices.ts
+++ src/modules/indicators/services/indicatorsServices.ts
@@ -2,7 +2,7 @@ import api from '@/utils/servicesUtils.ts'
 import type { IndicatorResponse } from '@/modules/indicators/types/indicatorsTypes.ts'
 
 const indicators = {
-  get: (): Promise<{ data: IndicatorResponse }> => api.get('/indicators'),
+  get: (): Promise<{ data: IndicatorResponse }> => api.get('/criteria'),
 }
 
 export default indicators
