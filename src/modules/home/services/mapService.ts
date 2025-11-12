d6baf7df9fff4b9f1f6b913b2e728e8294ec6edf
Augusto de Moraes Piatto <ampiatto@hotmail.com>
2025-11-12 19:14:57 -0300

    refactor: remove commentaries

--- src/modules/home/services/mapService.ts
+++ src/modules/home/services/mapService.ts
@@ -1,16 +1,6 @@
 import axios from 'axios'
 
-const randomLevel = () => Math.floor(Math.random() * 5) + 1
-
 export async function getRegionsLevel() {
-  // const response = await axios.get('/alert/per-region')
-  // return response.data
-  return [
-    { region_id: 1, level: randomLevel() },
-    { region_id: 2, level: randomLevel() },
-    { region_id: 3, level: randomLevel() },
-    { region_id: 4, level: randomLevel() },
-    { region_id: 5, level: randomLevel() },
-    { region_id: 6, level: randomLevel() },
-  ]
+  const response = await axios.get('/alert/per-region')
+  return response.data
 }
